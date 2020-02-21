9d2ebd9db57d510beec7d76a1e4ae1437d70d1b6
Sarah Carter <sarahmarie.carter@yahoo.com>
2020-02-21 12:43:29 -0800

    all classes pass all tests

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-<<<<<<< HEAD
-let employeeList = [];
+let managers = [];
+let interns = [];
+let engineers = [];
 
 function addEmployee() {
     inquirer.prompt(
@@ -50,61 +51,51 @@ function addEmployee() {
                 ]
             )
             .then(function(data) {
-                let employee = new Employee(data.name, data.email, data.id);
-                console.log(employee);
-                employeeList.push(employee);
-=======
-// inquirer.prompt(
-//     [
-//         {
-//             type: 'input',
-//             message: 'How many employees are there on this team?',
-//             name: 'num-employees'
-//         }
-//     ]
-// ).then(function(response) {
-//     let numEmployees = parseInt(response['num-employees']);
-//     for(let i=0; i<numEmployees; i++) {
-//         inquirer.prompt(
-//             [
-            //     {
-            //         type: 'input',
-            //         message: 'What is the employees name?',
-            //         name: 'name'
-            //     },
-            //     {
-            //         type: 'input',
-            //         message: 'What is their ID?',
-            //         name: 'id'
-            //     },
-            //     {
-            //         type: 'input',
-            //         message: 'What is their email?',
-            //         name: 'email'
-            //     },
-            //     {
-            //         type: 'input',
-            //         message: 'What is their role?',
-            //         name: 'job'
-            //     }
-            // ]
-//         ).then(function(data) {
-//             console.log(data);
-//             if(job === 'manager') {
-                
-//             }
-//         })
-//     }
-    
-// }).then(function(response) {
->>>>>>> f8bf0e068f2e5ac21b143e488d42485a5526848e
+                // let employee = new Employee(data.name, data.email, data.id);
+                // console.log(employee);
+                // employeeList.push(employee);
+                //console.log(data.role);
 
                 if(data.role == 'manager') {
-                    
+                    inquirer.prompt(
+                        {
+                            type: 'input',
+                            message: 'What is their office number?',
+                            name: 'number'
+                        }
+                    ).then(function(managerResponse) {
+                        let Jacob = new Manager(data.name, data.id, data.email, managerResponse.number);
+                        managers.push(Jacob);
+                    })
                 }
-
-<<<<<<< HEAD
-                addEmployee();
+                else if(data.role == 'intern') {
+                    inquirer.prompt(
+                        {
+                            type: 'input',
+                            message: 'What is their school?',
+                            name: 'school'
+                        }
+                    ).then(function(response) {
+                        let i = new Intern(data.name, data.id, data.email, response.school)
+                        console.log(i);
+                        interns.push(i);
+                    })
+                }
+                else if(data.role == 'engineer') {
+                    inquirer.prompt(
+                        {
+                            type: 'input',
+                            message: 'What is their Github username?',
+                            name: 'github'
+                        }
+                    ).then(function(response) {
+                        let e = new Engineer(data.name, data.id, data.email, response.github)
+                        console.log(e);
+                        engineers.push(e);
+                    })
+                }
+                
+                //addEmployee();
             })
         }
     })
@@ -112,50 +103,6 @@ function addEmployee() {
 
 addEmployee();
 
-function addManager() {
-    
-}
-=======
-inquirer.prompt(
-    {
-        type: 'confirm',
-        message: 'Would you like to add an employee?',
-        name: 'add-employee'
-    }
-).then(function(response) {
-    if(response) {
-        inquirer.prompt(
-            [
-                {
-                    type: 'input',
-                    message: 'What is the employees name?',
-                    name: 'name'
-                },
-                {
-                    type: 'input',
-                    message: 'What is their ID?',
-                    name: 'id'
-                },
-                {
-                    type: 'input',
-                    message: 'What is their email?',
-                    name: 'email'
-                },
-                {
-                    type: 'input',
-                    message: 'What is their role?',
-                    name: 'job'
-                }
-            ]
-        )
-        .then(function(data) {
-            if(data.role == 'manager') {
-                
-            }
-        })
-    }
-})
->>>>>>> f8bf0e068f2e5ac21b143e488d42485a5526848e
 
 //Would you like to add an employee?
 //function addEmployee()
